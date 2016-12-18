0b233e8316fd0ea41d92956b23feb6e29648358d
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2016-12-18 16:58:30 +0100

    white lists: suppresing fields binding added

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -8,6 +8,10 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -91,8 +95,22 @@ public class ProductController {
 	
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct){
+	public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct,
+			BindingResult result){
+		
+		String[] suppressedFields = result.getSuppressedFields();
+		if(suppressedFields.length > 0){
+			throw new RuntimeException("Binding of supressed fields error:" +
+					StringUtils.arrayToCommaDelimitedString(suppressedFields));
+		}
+		
 		productService.addProduct(newProduct);
 		return "redirect:/products";
 	}
+	
+	@InitBinder
+	public void initialiseBinder(WebDataBinder binder){
+		binder.setDisallowedFields("unitsInOrder", "discountinued");
+	}
+	
 }
